ec9213b8c9b0315360ab69dc05f966c4e6945e21
SaarthakTuli <saarthaktuli@gmail.com>
2022-10-08 01:37:01 +0530

    i

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js, jsx, ts, tsx}",
-    "./components/**/*.{js, ts, jsx, tsx}",
-    "./src/**/*.{js, jsx, ts, tsx}",
-  ],
+  content: ["./public/**/*.{ts, js, tsx, jsx}"],
   theme: {
     extend: {},
   },
